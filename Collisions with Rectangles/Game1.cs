bb9426f76b1b979dbac7ad42055f058d7ec4ca40
Hannah Anderson <339729345@GoTVDSB.ca>
2025-06-02 14:42:14 -0400

    Fixing coin!

--- Collisions with Rectangles/Game1.cs
+++ Collisions with Rectangles/Game1.cs
@@ -9,6 +9,30 @@ namespace Collisions_with_Rectangles
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        KeyboardState keyboardState;
+        MouseState mouseState;
+
+        Texture2D pacLeftTexture;
+        Texture2D pacRightTexture;
+        Texture2D pacUpTexture;
+        Texture2D pacDownTexture;
+
+        Texture2D currentPacTexture;
+        Rectangle pacRect;
+
+        Texture2D exitTexture;
+        Rectangle exitRect;
+
+        Texture2D barrierTexture;
+        Rectangle barrierRect1, barrierRect2;
+
+        Texture2D coinTexture;
+        Rectangle coinRect;
+
+        Rectangle window;
+
+        Vector2 pacSpeed;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,12 +44,33 @@ namespace Collisions_with_Rectangles
         {
             // TODO: Add your initialization logic here
 
+            pacSpeed = Vector2.Zero;
+            pacRect = new Rectangle(10, 10, 60, 60);
+
+            barrierRect1 = new Rectangle(0, 250, 350, 75);
+            barrierRect2 = new Rectangle(450, 250, 350, 75);
+
+            coinRect = new Rectangle(400, 50, coinTexture.Width, coinTexture.Height);
+            exitRect = new Rectangle(700, 380, 100, 100);
+
+            window = new Rectangle(0, 0, 800, 400);
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            pacDownTexture = Content.Load<Texture2D>("PacDown");
+            pacUpTexture = Content.Load<Texture2D>("PacUp");
+            pacRightTexture = Content.Load<Texture2D>("PacRight");
+            pacLeftTexture = Content.Load<Texture2D>("PacLeft");
+            currentPacTexture = pacRightTexture;
+
+            barrierTexture = Content.Load<Texture2D>("rockBarrier");
+            exitTexture = Content.Load<Texture2D>("hobbitDoor");
+            coinTexture = Content.Load<Texture2D>("coin");
 
             // TODO: use this.Content to load your game content here
         }
@@ -34,6 +79,31 @@ namespace Collisions_with_Rectangles
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            keyboardState = Keyboard.GetState();
+
+            pacSpeed = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacSpeed.X -= 2;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacSpeed.X += 2;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacSpeed.Y -= 2;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacSpeed.Y += 2;
+                currentPacTexture = pacDownTexture;
+            }
+            pacRect.Offset(pacSpeed);
 
             // TODO: Add your update logic here
 
@@ -45,6 +115,16 @@ namespace Collisions_with_Rectangles
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            _spriteBatch.Draw(coinTexture, coinRect, Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
